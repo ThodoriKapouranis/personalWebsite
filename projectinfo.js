baf059a6958ad44f8dd44fa2708a99d85bfe0ac7
Thodori Kapouranis <51767101+ThodoriKapouranis@users.noreply.github.com>
2019-08-22 21:55:35 -0400

    moved

--- projectinfo.js
+++
@@ -1,7 +0,0 @@
-projectTitle=["A test","Another Test","A longer longer longer test","haha",'wwww','ha ha ha ha ha ha ha ha ha'];
-
-projectDescription=[                                                    "Awwwwwww man this is gonna be  lot of text isn't it? It'll look very ugly when it extends all the way, Awwwwwww man this is gonna a lot of text isn't it? It'll look very ugly when it extends all the way",                                                                                   "Awwwwwww man this is gonna be  lot of text isn't it? It'll look very ugly when it extends all the way, Awwwwwww man this is gonna a lot of text isn't it? It'll look very ugly when it extends all the way",                                                                                         "Awwwwwww man this is gonna be  lot of text isn't it? It'll look very ugly when it extends all the way, Awwwwwww man this is gonna a lot of text isn't it? It'll look very ugly when it extends all the way",        "Wara", "Wara", "Wara"]
-
-projectDate=["June 19 2019", "June 19 2018", "June 18 2018", "XXX", "Xxx", ]
-
-projectImage=["https://i.gyazo.com/95c9645cbed8068a6383b963419ac2e5.png","","","","","",""]
