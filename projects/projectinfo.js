fa831ce4a4fba34bf4601d5b661655b23c17fb21
Thodori Kapouranis <51767101+ThodoriKapouranis@users.noreply.github.com>
2019-09-05 18:59:17 -0400

    Update projectinfo.js

--- projects/projectinfo.js
+++ projects/projectinfo.js
@@ -1,6 +1,6 @@
 projectTitle=["this website", "Japanese Quiz Site","A longer longer longer test","haha",'wwww','ha ha ha ha ha ha ha ha ha'];
 
-projectDescription=["How in the hell did i make this website i've already forgotten",                                                                                                             "Quizzes syllabry, and JLPT level specific words that are read off a google sheets which is imported from jisho.org",                                                                                                                                                                           "Awwwwwww man this is gonna be  lot of text isn't it? It'll look very ugly when it extends all the way, Awwwwwww man this is gonna a lot of text isn't it? It'll look very ugly when it extends all the way",        "Wara", "Wara", "Wara"]
+projectDescription=["Click me for the template explanation page I made"]                                                                                                            "Quizzes syllabry, and JLPT level specific words that are read off a google sheets which is imported from jisho.org",                                                                                                                                                                           "Awwwwwww man this is gonna be  lot of text isn't it? It'll look very ugly when it extends all the way, Awwwwwww man this is gonna a lot of text isn't it? It'll look very ugly when it extends all the way",        "Wara", "Wara", "Wara"]
 
 projectDate=["June 19 2019", "June 19 2018", "June 18 2018", "XXX", "Xxx", ]
 
@@ -10,9 +10,9 @@ projectRelPath=["projects/JLPT-Vocab-Info.html"]
 
 ///////////////////////////////////////////////////////////////////////////////
 
-resourceTitle=["Japanese"];
+resourceTitle=["Something Knowledgable"];
 
-resourceDescription=["A collection of all the resources I've personally come across studying Japanese"];
+resourceDescription=["A well thought out explanation I do not currently have"];
 
 resourceDate=["August 22 2019"];
 
