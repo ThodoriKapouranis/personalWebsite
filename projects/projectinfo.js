b5e6a0607e8e5bedf350a74fd678e145d319b4de
Thodori Kapouranis <51767101+ThodoriKapouranis@users.noreply.github.com>
2019-09-06 08:42:28 -0400

    Add files via upload

--- projects/projectinfo.js
+++ projects/projectinfo.js
@@ -1,12 +1,12 @@
 projectTitle=["this website", "Japanese Quiz Site","A longer longer longer test","haha",'wwww','ha ha ha ha ha ha ha ha ha'];
 
-projectDescription=["Click me for the template explanation page I made"]                                                                                                            "Quizzes syllabry, and JLPT level specific words that are read off a google sheets which is imported from jisho.org",                                                                                                                                                                           "Awwwwwww man this is gonna be  lot of text isn't it? It'll look very ugly when it extends all the way, Awwwwwww man this is gonna a lot of text isn't it? It'll look very ugly when it extends all the way",        "Wara", "Wara", "Wara"]
+projectDescription=["Click me for the template explanation page I made",                                                                                                            "Quizzes syllabry, and JLPT level specific words that are read off a google sheets which is imported from jisho.org",                                                                                                                                                                           "Awwwwwww man this is gonna be  lot of text isn't it? It'll look very ugly when it extends all the way, Awwwwwww man this is gonna a lot of text isn't it? It'll look very ugly when it extends all the way",        "Wara", "Wara", "Wara"];
 
-projectDate=["June 19 2019", "June 19 2018", "June 18 2018", "XXX", "Xxx", ]
+projectDate=["June 19 2019", "June 19 2018", "June 18 2018", "XXX", "Xxx", ];
 
-projectImage=["https://i.gyazo.com/95c9645cbed8068a6383b963419ac2e5.png","/icons/jlptvocab.png","","","","",""]
+projectImage=["https://i.gyazo.com/95c9645cbed8068a6383b963419ac2e5.png","/icons/jlptvocab.png","","","","",""];
 
-projectRelPath=["projects/JLPT-Vocab-Info.html"]
+projectRelPath=["projects/JLPT-Vocab-Info.html"];
 
 ///////////////////////////////////////////////////////////////////////////////
 
